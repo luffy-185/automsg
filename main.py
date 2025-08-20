04912a389603a40297e451336fdef823ab17636d
luffy-185 <userdead2323@gmail.com>
2025-08-20 18:05:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ except ImportError:
 
 # ===== CONFIG =====
 API_ID = int(os.environ.get("API_ID", "0"))
-API_HASH = os.environ.get("API_HASH", ""))
+API_HASH = os.environ.get("API_HASH", "")
 SESSION = os.environ.get("SESSION", "")
 OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
 

0671a380690834e3d65e3d9ba890a5bc9da6c8c8
luffy-185 <userdead2323@gmail.com>
2025-08-22 19:44:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from keep_alive import keep_alive
 # ===== CONFIG =====
 API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
-SESSION = os.environ.get("SESSION", "")
+SESSION = os.environ.get("SESSION_STRING", "")
 OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
 
 # ===== LOGGING =====
